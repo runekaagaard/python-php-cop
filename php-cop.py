4b8315f9c8c67b2e6108077d96b316b8624c3a87
runekaagaard <rumi.kg@gmail.com>
2011-09-22 22:03:45 +0200

    Cleaning up some. Now its time to make the php bootstrap file, that sets some xdebug formatting settings and then write a proper parser for that format.

--- php-cop.py
+++ php-cop.py
@@ -3,10 +3,11 @@ import os
 import re
 import sys
 import time
+import argparse
 
 class PhpCop(object):
-    def __init__(self):
-        self.file = sys.argv[1]
+    def __init__(self, args):
+        self.args = args
         self.init_urwid()
     
     def init_urwid(self):
@@ -18,44 +19,64 @@ class PhpCop(object):
             ('bg', 'black', 'dark blue')
         ])
 
-        self.frame = urwid.Frame(self.get_columns(), "", "", focus_part='body')
+        self.frame = urwid.Frame(self.render_columns(), "", "", focus_part='body')
         self.box = urwid.AttrWrap(self.frame, 'default')
         self.ui.run_wrapper(self.main)
-
-    def get_columns(self):
-        errors = []
-        for line in open(self.file).readlines():
-            if line.strip() == "":
-                continue
-            line = line.strip()
-            if "on line" in line:
-                stacktrace = []
-                errors.append((line, stacktrace))
-            else:
-                stacktrace.append(line)
         
-        parts = []
-        i = 0
-        for (error, stacktraces) in errors:
-            if "Notice:" in error:
-                color = "default"
-            else:
-                color = "warn"
-            parts.append(urwid.AttrWrap(urwid.Text(error, wrap='clip'), color))
-            for s in stacktraces:
-                parts.append(urwid.AttrWrap(urwid.Text(s, wrap='clip'), "stacktrace"))
-            i += 1
-        return urwid.Columns([urwid.ListBox(parts)], dividechars=1, focus_column=0)
-
     def main(self):
         while True:
             self.size = self.ui.get_cols_rows()
             self.canvas = self.box.render(self.size)
-            self.frame.set_body(self.get_columns())
+            self.frame.set_body(self.render_columns())
             self.ui.draw_screen(self.size, self.canvas)
             time.sleep(0.5)
-PhpCop()
+    
+    def parse_file(self):
+        file_structure = []
+        for line in self.args.logfile.readlines():
+            if line.strip() == "":
+                continue
+            line = line.strip()
+            
+            if "on line" in line:
+                stacktrace = []
+                file_structure.append((line, stacktrace))
+            else:
+                stacktrace.append(line)
+        
+        self.args.logfile.seek(0)
+        return file_structure
+    
+    def render_columns(self):
+        def get_color(error):
+            if "Notice:" in error:
+                return "default"
+            else:
+                return "warn"
+        
+        def render_error(error):
+            color = get_color(error)
+            return urwid.AttrWrap(urwid.Text(error, wrap='clip'), color)
+            
+        def render_stacktrace(stacktrace):
+            return urwid.AttrWrap(urwid.Text(stacktrace, wrap='clip'), 
+                                  "stacktrace")
+                
+        columns = []
+        for (error, stacktraces) in self.parse_file():
+            columns.append(render_error(error))
+            for stacktrace in stacktraces:
+                columns.append(render_stacktrace(stacktrace))
+        
+        return urwid.Columns([urwid.ListBox(columns)], dividechars=1, 
+                             focus_column=0)
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Watch them PHP errors.')
+    parser.add_argument('logfile', type=file, help="The logfile to watch")
+    args = parser.parse_args()
+    PhpCop(args)
+    
 '''
 def exit_on_q(input):
 	global current_open_stacktrace, ignores
