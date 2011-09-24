97f303e89b3eaffbf5eab0140d7fb691b8f3bd06
runekaagaard <rumi.kg@gmail.com>
2011-09-24 20:41:16 +0200

    Basic colored output works. But scrolling is tricking me out. Good night!

--- php-cop.py
+++ php-cop.py
@@ -15,13 +15,15 @@ class PhpCop(object):
         self.ui = urwid.curses_display.Screen()
         self.ui.register_palette([
             ('default', 'default', 'default'),
-            ('warn', 'light red', 'default'),
-            ('date', 'dark gray', 'default'),
-            ('level', 'dark green', 'default'),
-            ('content', 'default', 'default'),
+            ('Notice', 'light green', 'default'),
+            ('Warning', 'light red', 'default'),
+            ('Fatal error', 'dark red', 'light gray'),
+            ('date', 'default', 'default'),
+            ('content', 'yellow', 'default'),
             ('file', 'brown', 'default'),
             ('lineno', 'light cyan', 'default'),
-            ('stacktrace', 'light green', 'default'),
+            ('stacktrace_title', 'light green', 'default'),
+            ('stacktrace_content', 'dark green', 'default'),
             ('bg', 'black', 'dark blue')
         ])
 
@@ -72,28 +74,39 @@ class PhpCop(object):
         return file_structure
     
     def render_columns(self):
-        def stack_title(match): return None
-        def stack_content(match): return None
+        def stack_title(gd):
+            return None
+            return urwid.Text([
+                ('default', '    '),
+                ('stacktrace_title', 'Stacktrace:')
+            ])
+                
+        def stack_content(gd):
+            return urwid.Padding(urwid.Text([
+                ('stacktrace_content', gd['content']),
+                ('default', ' in '),
+                ('file', gd['file']),
+                ('default', ':'),
+                ('lineno', gd['lineno']),
+            ]), left=4)
+        
         def func_error(match): return None
         
         def format_date(date):
             return date[-8:]
             
         def error(gd):
-            ##color = get_color(error)
-            #print gd; sys.exit()
             return urwid.Text([
                 ('date', format_date(gd['date'])),
                 ('default', ' '),
-                ('level', gd['level']),
+                (gd['level'], gd['level']),
                 ('default', ': '),
                 ('content', gd['content']),
-                ('content', '. '),
+                ('default', ' in '),
                 ('file', gd['file']),
                 ('default', ':'),
                 ('lineno', gd['lineno']),
             ])
-            return urwid.AttrWrap(urwid.Text("YO", wrap='clip'), 'default')
         
         def other(match): return None 
         
@@ -108,11 +121,15 @@ class PhpCop(object):
             column = funcs[line_type](match.groupdict())
             if column:
                 columns.append(column)
-            for stacktrace in stacktraces:
-                pass
+            for (line_type, match) in stacktraces:
+                column = funcs[line_type](match.groupdict())
+                if column:
+                    columns.append(column)
         
-        return urwid.Columns([urwid.ListBox(columns)], dividechars=1, 
-                             focus_column=0)
+        lb = urwid.ListBox(urwid.SimpleListWalker([urwid.AttrMap(c, None, 
+                             'reveal focus') for c in columns]))
+        #lb.set_focus(30)
+        return lb
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Watch them PHP errors.')
